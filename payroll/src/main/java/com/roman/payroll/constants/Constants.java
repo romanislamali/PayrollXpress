d8f6a85b8ff718d8909b53fcce134ea5df433407
Md. Roman Islam Ali <romanislam897@gmail.com>
2023-12-26 17:06:41 +0600

    add new constant

--- payroll/src/main/java/com/roman/payroll/constants/Constants.java
+++ payroll/src/main/java/com/roman/payroll/constants/Constants.java
@@ -1,8 +1,11 @@
 package com.roman.payroll.constants;
 
 public interface Constants {
+
+    String USER_REGISTER = "/user/register";
     String ACCOUNT_TYPE = "/account_type";
     String BANK_ACCOUNT = "/bank_account";
     String EMPLOYEE = "/employee";
     String SALARY_GRADE = "/salary_grade";
+
 }
